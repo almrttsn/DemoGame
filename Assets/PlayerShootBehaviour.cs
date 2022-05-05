eb17f4dcca89c62de337bdd97b4ad1977459e278
almrttsn <88113243+almrttsn@users.noreply.github.com>
2022-05-05 12:21:13 +0300

    Player pull trigger process implemented

---
+++ Assets/PlayerShootBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject _bullet;
+    [SerializeField] private Transform _playerGunBarrelPoint;
+
+    public int _shootPower;
+
+    void Update()
+    {
+        PlayerPullTriggerProcess();
+    }
+
+    private void PlayerPullTriggerProcess()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var bullet = Instantiate(_bullet, _playerGunBarrelPoint.transform.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody>().AddForce(_playerGunBarrelPoint.transform.forward * _shootPower);
+        }
+    }
+}
