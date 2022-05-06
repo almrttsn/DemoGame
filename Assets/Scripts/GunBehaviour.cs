31046c23b09aa8675c3f0f543ef12cfe8999135f
almrttsn <88113243+almrttsn@users.noreply.github.com>
2022-05-06 12:10:39 +0300

    gun behaviour added

---
+++ Assets/Scripts/GunBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBehaviour : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _buckshots;
+
+    private PlayerController _playerController;
+    private Transform _buckshotRandomCreationTransform;
+    private float _randomRange;
+
+    public void Initialize(PlayerController playerController)
+    {
+        _playerController = playerController;
+        BuckshotCreationProcess();
+    }
+
+    public void BuckshotCreationProcess()
+    {
+        for(int i = 0; i<_buckshots.Count; i++)
+        {
+            _randomRange = Random.Range(-1f, 1f);
+            _buckshotRandomCreationTransform.position = new Vector3(0, _randomRange, 0);
+            Debug.Log(_randomRange);
+            //var _buckshot = Instantiate(_buckshots, _buckshotRandomCreationTransform.position, Quaternion.identity);
+        }
+    }
+
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,12 @@ public class PlayerController : MonoBehaviour
     public GameManager GameManager;
     public PlayerShootBehaviour PlayerShootBehaviour;
     public PlayerMovementBehaviour PlayerMovementBehaviour;
+    public GunBehaviour GunBehaviour;
 
     public void Initialize(GameManager gameManager)
     {
         PlayerMovementBehaviour.Initialize(this);
         PlayerShootBehaviour.Initialize(this);
+        GunBehaviour.Initialize(this);
     }
 }
